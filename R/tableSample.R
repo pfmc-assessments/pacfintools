c5c64b0ab83cd0f6afa2751aee9d3a44e483a960
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-04-09 11:13:02 -0700

    revise tableSample to be more robust and improve usability
    This work is linked to #115

--- R/tableSample.R
+++ R/tableSample.R
@@ -4,61 +4,120 @@
 #' be included in an assessment document.
 #'
 #' @inheritParams cleanPacFIN
-#' @inheritParams writeComps
-#' @param strat A vector of column names to stratify the output over. For
+#' @param Pdata  A data frame returned from [cleanPacFIN()], [get_expanded_comps()],
+#'   [getExpansion_1()], [getExpansion_2], or [getComps()]. If a data frame returned
+#'   from [getComps()] is provided a table of samples by year and fleet with trips,
+#'   samples, and input sample sizes will be returned.  If a data frame returned from
+#'   the other functions listed, a table of samples by year and `stratification.cols`
+#'   for lengths and ages will be returned.
+#' @param savedir A file path to the directory where the results will be saved.
+#'   The default is NULL.
+#' @param stratification.cols Columns in `Pdata` to summarize the data for the
+#'   calculation of samples sizes.  These columns are not used in the data
+#'   frame is from [getComps()] because the grouping structure is already set in
+#'   that data frame.
+#' @param strat Deprecated. A vector of column names to stratify the output over. For
 #'   example, if you wish to summarize by ageing method, then the argument
 #'   would look like `strat = "agemethod"` or, if you want to look at fleets
 #'   and gear, `strat = c("fleet", "usegear")`.
-#' @param comps Specify whether to calculate the length or Age samples.
+#' @param comps Deprecated. Specify whether to calculate the length or Age samples.
 #' The default is to calculate the number of length samples.
-#' @param remove_yrs A vector of years to remove from the data before
+#' @param remove_yrs Deprecated. A vector of years to remove from the data before
 #' summarizing sample sizes. The default of \code{NULL} leads to no
 #' sub setting of the data.
+#' @param fname Deprecated. A filename with the appropriate extension, used to save the
+#'   function output to the disk. Full, relative, or simple paths are
+#'   allowed because the argument is used as is, i.e., not redirected to a
+#'   directory different than [getwd()].
 #'
 #' @return
-#' A table is written to a csv file as specified in \code{fname} and the data frame
-#' is also returned as an object invisibly.
-
+#' A data frame is returned of samples by year
+#'
 #' @author Chantel R. Wetzel
 #' @export
-
+#'
 tableSample <- function(
   Pdata,
-  fname = paste0("fishery_", comps, "_samples.csv"),
-  strat = "AGENCY_CODE",
-  comps = c("LEN", "AGE"),
-  remove_yrs = NULL
+  stratification.cols = c("state", "geargroup"),
+  savedir = NULL,
+  verbose = TRUE,
+  comps = lifecycle::deprecated(),
+  remove_yrs = lifecycle::deprecated(),
+  fname = lifecycle::deprecated(),
+  strat = lifecycle::deprecated()
 ) {
-  Pdata$strat <- apply(Pdata[, strat, drop = FALSE], 1, paste0, collapse = ".")
-  comps <- match.arg(comps, several.ok = FALSE)
-  if (comps == "LEN") {
-    temp <- Pdata[!is.na(Pdata$FISH_LENGTH), ]
+  if (lifecycle::is_present(fname)) {
+    lifecycle::deprecate_warn(
+      when = "0.4.3",
+      what = "tableSample(fname = )",
+      details = "fname is deprecated, files are automatically named internally"
+    )
+  }
+  if (lifecycle::is_present(strat)) {
+    lifecycle::deprecate_warn(
+      when = "0.4.3",
+      what = "tableSample(strat = )",
+      details = "strat is deprecated, please use stratification.cols"
+    )
+    stratification.cols <- strata
+  }
+  if (lifecycle::is_present(remove_yrs)) {
+    lifecycle::deprecate_warn(
+      when = "0.4.3",
+      what = "tableSample(strat = )",
+      details = "remove_yrs is deprecated, please filter data outside of function"
+    )
+  }
+  if (lifecycle::is_present(comps)) {
+    lifecycle::deprecate_warn(
+      when = "0.4.3",
+      what = "tableSample(comps = )",
+      details = "comp is deprecated, if passing an object from the cleanPacFIN or 
+      either of the expansion function will result in a table of lengths and ages, 
+      otherwise, a table of trips, fish, and input sample size will be created 
+      for either lengths or ages dependent upon the data type in the getComps() object."
+    )
+  }
+  nwfscSurvey:::check_dir(dir = savedir, verbose = verbose)
+
+  # Evaluate whether the user wants raw samples sizes of summary of processed comps
+  if ("n_stewart" %in% colnames(Pdata)) {
+    file_name <- "commercial_trips_and_input_sample_sizes.csv"
+    samples <- Pdata |>
+      tibble::as_tibble() |>
+      dplyr::summarise(
+        .by = c(fishyr, fleet),
+        trips = sum(unique(n_tows)),
+        samples = sum(unique(n_fish)),
+        input_n = sum(unique(n_stewart))
+      ) |>
+      dplyr::rename(
+        year = fishyr
+      ) |>
+      dplyr::arrange(fleet) |>
+      as.data.frame()
+  } else {
+    file_name <- "commercial_length_age_samples.csv"
+    samples <- Pdata |>
+      tibble::as_tibble() |>
+      dplyr::summarise(
+        .by = dplyr::all_of(c("fishyr", stratification.cols)),
+        n_length = sum(!is.na(lengthcm)),
+        n_age = sum(!is.na(Age))
+      ) |>
+      dplyr::rename(
+        year = fishyr
+      ) |>
+      as.data.frame()
   }
 
-  if (comps == "AGE") {
-    temp <- Pdata[!is.na(Pdata$Age), ]
+  if (!is.null(savedir)) {
+    utils::write.csv(
+      x = samples,
+      file = file.path(savedir, file_name),
+      row.names = FALSE
+    )
   }
 
-  if (!is.null(remove_yrs)) {
-    temp <- temp[!temp$SAMPLE_YEAR %in% remove_yrs, ]
-  }
-
-  Ntow <- table(
-    temp$SAMPLE_YEAR,
-    temp$strat,
-    !duplicated(as.character(temp$SAMPLE_NUMBER))
-  )[, , "TRUE"]
-  Nfish <- table(temp$SAMPLE_YEAR, temp$strat)
-
-  samples <- rownames(Ntow)
-  names <- "Year"
-  for (a in colnames(Ntow)) {
-    get <- cbind(Ntow[, a], Nfish[, a])
-    samples <- cbind(samples, get)
-    names <- c(names, paste0(a, ".tows"), paste0(a, ".fish"))
-  }
-  colnames(samples) <- names
-
-  utils::write.csv(samples, file = fname, row.names = FALSE)
-  return(invisible(samples))
+  return(samples)
 }
