3716939d74039a64d65fc3a0462b288d53488bdb
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-04-13 08:02:22 -0700

    add agency port code data

--- data-raw/PacFIN_Ports_withlatlong.R
+++ data-raw/PacFIN_Ports_withlatlong.R
@@ -3,7 +3,9 @@ pacfin_ports_withlatlong_raw <- tibble::as_tibble(read.csv(
   file.path("data-raw", "PacFIN_Ports_withlatlong.csv")
 ))
 
-colnames(pacfin_ports_withlatlong_raw) <- tolower(colnames(pacfin_ports_withlatlong_raw))
+colnames(pacfin_ports_withlatlong_raw) <- tolower(colnames(
+  pacfin_ports_withlatlong_raw
+))
 
 pacfin_ports_withlatlong <- pacfin_ports_withlatlong_raw |>
   labelled::set_variable_labels(
@@ -15,3 +17,21 @@ pacfin_ports_withlatlong <- pacfin_ports_withlatlong_raw |>
     latitude = "Latitude of the port location with minutes and seconds in radians."
   )
 usethis::use_data(pacfin_ports_withlatlong, overwrite = TRUE)
+
+agency_ports_withlatlong_raw <- tibble::as_tibble(read.csv(
+  file.path("data-raw", "Ports_Crosswalk_2024-07-17.csv")
+))
+colnames(agency_ports_withlatlong_raw) <- tolower(colnames(
+  agency_port_withlatlong_raw
+))
+
+agency_ports_withlatlong <- agency_ports_withlatlong_raw |>
+  labelled::set_variable_labels(
+    agid_port = "Agency port abbreviation.",
+    agency_portdescription = "Agency port description",
+    longitude = "Longitude of the port location with minutes and seconds in radians.",
+    latitude = "Latitude of the port location with minutes and seconds in radians.",
+    area = "North or south of 40.10 N. latitude",
+    ifq_area = "IFQ area code used by the West Coast Groundfish Observer Program."
+  )
+usethis::use_data(agency_ports_withlatlong, overwrite = TRUE)
